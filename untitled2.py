000b7ede7124bb61b7069981f484970a854c2eb8
tushar <sharmatushar601@gmail.com>
2020-06-07 12:31:35 +0530

    uploaded scarper and run code

---
+++ untitled2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  7 09:17:04 2020
+
+@author: asus
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+path = "C:/Users/asus/Documents/ds_salary_proj/chromedriver"
+df = gs.get_jobs('data scientist', 15, False, path , 15 )
+df
